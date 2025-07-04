f27ce6e84565262c53bd4907cbd13e6d5ff6609c
onineko <70475483+s-onineko@users.noreply.github.com>
2025-07-04 14:32:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,11 @@ for pkg in nltk_packages:
     except LookupError:
         nltk.download(pkg)
 
-
 st.set_page_config(layout="wide")
-st.title("PROMPT LIBRARY")
+st.markdown(
+    "<div style='font-size:48px; font-weight:bold; color:#C3002F;'>PROMPT LIBRARY</div>",
+    unsafe_allow_html=True
+)
 st.write(" #### S&A GenAI Workshop- ORIGINAL SYSTEM / フレーズ抽出・辞書化システム")
 st.write("***")
 uploaded_file = st.file_uploader("CSVファイルをアップロードしてください（user, type, prompt列）", type=["csv"])
